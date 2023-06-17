87482e5d84aa89ae1eece0832ef289de3c1ea161
manielsen13 <marshall.niels@gmail.com>
2023-06-17 18:09:15 -0400

    Create rough layout of home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import { useState } from "react";
+import clockImg from "./assets/clock.png";
+import resetImg from "./assets/reset.png";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [time, setTime] = useState(360);
+  const [exercises, setExercises] = useState([]);
 
   return (
     <>
-      <p>Hello World</p>
+      <button className="absolute select-none right-7 top-5">Options</button>
+
+      <>
+        <div className="flex flex-col justify-center items-center gap-5">
+          <img src={clockImg} className="w-5 h-auto mt-24 select-none"></img>
+          <label className="select-none">{`${
+            ("%02d", Math.floor(time / 60))
+          }:${(time % 60).toLocaleString("en-US", {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+          })}`}</label>
+          <img src={resetImg} className="w-5 h-auto select-none"></img>
+
+          <button className="select-none">Start</button>
+          <button className="select-none">Stop</button>
+        </div>
+      </>
+
+      <>
+        <div className="flex justify-around mt-20">
+          <div className="flex text-center flex-col gap-7">
+            <label className="select-none">Exercise</label>
+            <label className="select-none">Exercise Name</label>
+          </div>
+          <div className="flex flex-col text-center gap-7">
+            <label className="select-none"># Sets Complete</label>
+            <div className="flex gap-5 justify-center">
+              <button className="select-none">-</button>
+              <label className="select-none">0</label>
+              <button className="select-none">+</button>
+            </div>
+          </div>
+        </div>
+      </>
     </>
   );
 }
